734befb94db3f55030210c97a8454049ad9ae23c
vannessadac-code <vannessadac@gmail.com>
2025-12-19 00:03:03 -0500

    Bloquear el giro de la aplicacion

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ void main() async {
     debugPrint('Failed to initialize database: $e');
   }
 
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   runApp(const App());
 }
